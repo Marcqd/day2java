866b27024f067654a1908150f0e8234c663a91c7
Marc Zhang <sjetqd@gmail.com>
2024-01-16 10:52:46 -0700

    new file

---
+++ 116/complexdatatype.js
@@ -0,0 +1,44 @@
+/*const userOne = {
+  userId:1234,
+  firstName:"Ashlyn",
+  lastName:"Knox",
+  isActive:true,
+}
+const userTwo = {
+  userId:1235,
+  firstName:"Copernicus",
+  lastName:"sees the stars",
+  isActive:false,
+}
+const userThree = {
+  userId:1236,
+  firstName:"Sally",
+  lastName:"Fields",
+  isActive:true,
+}*/
+//const users =[userOne, userTwo, userThree];
+
+const users =[
+  {
+    userId:1234,
+    firstName:"Ashlyn",
+    lastName:"Knox",
+    isActive:true,
+  },
+  {
+    userId:1235,
+    firstName:"Copernicus",
+    lastName:"sees the stars",
+    isActive:false,
+  },
+  {
+    userId:1236,
+    firstName:"Sally",
+    lastName:"Fields",
+    isActive:true,
+  }
+
+]
+
+console.log(users[]);
+
---
+++ 116/index.js
@@ -0,0 +1,19 @@
+const firstName = 'Ashlyn';
+const lastName = 'Knox';
+let isActive = true;
+const userId= 2123;
+
+// single log at a time
+console.log(firstName);
+console.log(lastName);
+//log multiple values
+
+console.log(userId,isActive);
+//log mixed values
+console.log("User: " + firstName + " is " + userId);
+
+//template literales
+console.log(`User ${userId}: ${firstName} ${lastName} ${isActive}. zaboomafoo`);
+console.log(`User ${userId}: ${firstName} ${lastName} ${isActive}. Zaboomafoo`);
+
+
